d648ce1c828f208baf88d6ef491ef203222bb6ce
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-08 22:22:59 -0700

    started inquirer

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['']
+        }
+    ])
