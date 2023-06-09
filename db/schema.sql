765670ee6f2ef832678c1073bdc7824efc645829
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-08 22:15:54 -0700

    added more seed data

--- db/schema.sql
+++ db/schema.sql
@@ -39,7 +39,11 @@ INSERT INTO Role (title, salary, department_id)
 VALUES  ('Chief Financial Officer', 428169.00, 1),
         ('Financial Analyst', 95570.00, 1),
         ('Financial Advisor', 94170.00, 1),
-        ('Accountant', 77250.00, 1);
+        ('Accountant', 77250.00, 1),
+        ('Senior Developer', 102876.00, 2),
+        ('Junior Developer', 83876.00, 2),
+        ('Registered Nurse', 82750.00, 3),
+        ('Certified Nurse Assistant', 34287.00, 3);
 
 INSERT INTO Employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Callie', 'Bridges', 1, null),
@@ -47,7 +51,13 @@ VALUES  ('Callie', 'Bridges', 1, null),
         ('Anne', 'Montgomery', 3, 1),
         ('Bruce', 'Rios', 4, 2),
         ('Steven', 'Jacobs', 4, 2),
-        ('Jonathan', 'McClain', 4, 2);
+        ('Jonathan', 'McClain', 4, 2),
+        ('Ben', 'Cordova', 5, null),
+        ('Emelia', 'Whitney', 6, 7),
+        ('Magnus', 'Evans', 6, 7),
+        ('Bernard', 'Hoover', 7, null),
+        ('Hallie', 'Camacho', 8, 10),
+        ('Oliwier', 'Trujillo', 8, 10);
 
 
 SELECT * FROM Department;
